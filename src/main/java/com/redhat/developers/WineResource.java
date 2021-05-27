2f73c1cbad22f6f56fd1c618e6b2a61cb7e10a6c
Eduardo <eduardomelle@gmail.com>
2021-05-27 20:02:36 -0300

    Implementaçoes referentes a aula Quarkus II: Kubernetes Native Deep Dive.

---
+++ src/main/java/com/redhat/developers/WineResource.java
@@ -0,0 +1,28 @@
+package com.redhat.developers;
+
+import javax.inject.Inject;
+import javax.json.bind.JsonbBuilder;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.jboss.resteasy.annotations.SseElementType;
+
+import io.smallrye.mutiny.Multi;
+
+@Path("/wine")
+public class WineResource {
+
+  @Channel("priced-wine")
+  Multi<String> pricedWines;
+
+  @GET
+  @Produces(MediaType.SERVER_SENT_EVENTS)
+  @SseElementType(MediaType.APPLICATION_JSON)
+  public Multi<PricedWine> wines() {
+    return pricedWines.map(s -> JsonbBuilder.create().fromJson(s, PricedWine.class));
+  }
+
+}
